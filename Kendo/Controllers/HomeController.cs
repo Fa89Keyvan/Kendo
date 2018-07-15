738727d47ea369fe5dcd49081aa3778b6032ce0e
k.fathabadi <k.fathabadi@DESKTOP-1VMUBML>
2018-07-15 10:56:51 +0430

    firstCommit

---
+++ Kendo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Kendo.Models.Kendo;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kendo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Persons()
+        {
+            var gridRequest = new GridRequest(Request.QueryString);
+            var sort = gridRequest.Sorts.Any() ? gridRequest.Sorts[0] : null;
+            var list = Person.GetList();
+            
+            var model = list.OrderBy(sort).Skip(gridRequest.skip).Take(gridRequest.take);
+
+            return Json(new { HasError = false,Data = new { List = model }, total = list.Count() },JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<Person> GetList()
+        {
+            var lst = new List<Person>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                lst.Add(new Person { Id = i, Name = $"Name {i}" });
+            }
+            return lst;
+        }
+    }
+}
---
+++ Kendo/Extentions/LinqExtentions.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class LinqExtentions
+    {
+        public static IOrderedEnumerable<TSource> OrderBy<TSource>
+            (this IEnumerable<TSource> source,Sort sort)
+        {
+            sort = sort ?? new Sort() { Field = string.Empty, Direction = SortDirection.AscSort };
+
+            if (string.IsNullOrEmpty(sort.Field))
+                return source.OrderBy(x => x.GetType().GetProperties()[0].GetValue(x, null));
+
+            return (sort.Direction == SortDirection.AscSort)
+                ? source.OrderBy(x => x.GetType().GetProperty(sort.Field).GetValue(x, null))
+                : source.OrderByDescending(x => x.GetType().GetProperty(sort.Field).GetValue(x, null));
+        }
+
+        
+    }
+    public class SortDirection
+    {
+        private static SortDirection _ascSort = new SortDirection() { Direction = "ASC" };
+        private static SortDirection _dscSort = new SortDirection() { Direction = "DSC" };
+
+        public static SortDirection AscSort => _ascSort;
+        public static SortDirection DscSort => _dscSort;
+
+        private SortDirection() { }
+
+        public string Direction { get; private set; }
+
+        public static bool operator ==(SortDirection right, SortDirection left)
+        {
+            return right.Direction == left.Direction;
+        }
+        public static bool operator !=(SortDirection right, SortDirection left)
+        {
+            return right.Direction != left.Direction;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var direction = obj as SortDirection;
+            return direction != null &&
+                   Direction == direction.Direction;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    public class Sort
+    {
+        public string Field { get; set; }
+        public SortDirection Direction { get; set; }
+
+    }
+}
