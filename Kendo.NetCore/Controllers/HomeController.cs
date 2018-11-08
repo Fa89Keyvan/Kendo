09d3255922d531053e880b880e109947049c0948
keyvan fa <keyvan.fa89@live.com>
2018-11-08 07:59:34 +0330

    add netcore

---
+++ Kendo.NetCore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kendo.NetCore.Models;
+
+namespace Kendo.NetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Persons()
+        {
+            var model = Models.Person.GetList();
+            return Json(new { Data = model, Total = model.Count() });
+        }
+    }
+}
---
+++ Kendo.NetCore/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kendo.NetCore.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        private static List<Person> _list;
+
+        public static List<Person> GetList()
+        {
+            if(_list == null)
+            {
+                _list = new List<Person>();
+                for (int i = 0; i < 100; i++)
+                {
+                    _list.Add(new Person { Id = i, Name = $"Name {i}", Age = new Random().Next(18, 99) });
+                }
+            }
+            
+            return _list;
+        }
+    }
+}
